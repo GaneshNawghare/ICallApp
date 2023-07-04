24783fbe66d7d4574fc57b961ad97ed185b9f18a
GaneshNawghare <ganesh7003nawghare@gmail.com>
2023-07-04 16:55:01 +0530

    added api calling for getting data of sos and content inside part

--- axios/index.ts
+++ axios/index.ts
@@ -6,8 +6,10 @@ export const getContentData =  () =>customAxios.get('/api/content/getContent');
 
 export const getSosInData =  (id:any) => customAxios.get(`/api/sosIn/get-SosParentIn/${id}`);
 
-
 export const getContentInnerData =  (id:any) => customAxios.get(`/api/contentIn/get-ContentParentIn/${id}`);
 
-
 export const getContentInData =  () => customAxios.get('/api/contentIn/getContentIn');
+
+export const getContentInOneData =  (id:any) => customAxios.get(`/api/contentIn/get-ContentIn/${id}`);
+
+export const getSosInOneData =  (id:any) => customAxios.get(`/api/sosIn/get-SosIn/${id}`);
