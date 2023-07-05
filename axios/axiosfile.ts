b9e7cc32c255bebf44d8d97fa3a44889089e1cc6
GaneshNawghare <ganesh7003nawghare@gmail.com>
2023-07-05 23:15:05 +0530

    added time duration for getting api response

--- axios/axiosfile.ts
+++ axios/axiosfile.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
+const TIMEOUT_DURATION = 15000; //15 sec
 // console.log("process",process.env.BACKEND_SOCKET_URL);
 const customAxios = axios.create({
   // baseURL: process.env.BACKEND_SOCKET_URL,
   // console.log('process.env.BASE_URL')
   // baseURL: process.env.BASE_URL,
   baseURL: `https://icall-socket-service-dev-ht7hh6taza-as.a.run.app`,
+  timeout: TIMEOUT_DURATION,
 })
 
 customAxios.interceptors.request.use(
@@ -22,6 +24,10 @@ customAxios.interceptors.response.use(
       return response;
     },
     function (error) {
+      if (error.code === 'ECONNABORTED' && error.message.includes('timeout')) {
+        // Handle network timeout error
+        return Promise.reject(new Error('Network request timed out.'));
+      }
       return Promise.reject(error);
     },
   );
